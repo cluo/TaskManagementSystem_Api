1e59ecb95d255f6903d57db9bb07cc2785054bff
radiantwf <radiantwf@gmail.com>
2016-10-30 21:06:39 +0800

    版本号修改

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker kill api;docker rm api;
-docker rmi api-image 211.157.146.6:5000/task-management-api:0.01
+docker rmi api-image 211.157.146.6:5000/task-management-api
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build *.go
 docker restart TM-mongo
 docker build -t api-image .
-docker tag api-image 211.157.146.6:5000/task-management-api:0.01
-docker push 211.157.146.6:5000/task-management-api:0.01
+docker tag api-image 211.157.146.6:5000/task-management-api
+docker push 211.157.146.6:5000/task-management-api
 docker rmi api-image
-docker run -d --link "TM-mongo:mongo" -p 6001:6001 --name=api 211.157.146.6:5000/task-management-api:0.01
+docker run -d --link "TM-mongo:mongo" -p 6001:6001 --name=api 211.157.146.6:5000/task-management-api
