fac9fb6803d2cf3888719c3e025f9c969423f963
王峰 <wangfeng@wangfengdeMac-mini.local>
2016-11-10 18:35:17 +0800

    发布版批处理修改

--- 172.16.2.26.sh
+++ 172.16.2.26.sh
@@ -1,7 +1,7 @@
-docker kill task-management-api oauth-server-service task-management-frontend ambassador-mongo ambassador-oauthdb;docker rm task-management-api oauth-server-service task-management-frontend ambassador;
-docker rmi 211.157.146.6:5000/task-management-api 211.157.146.6:5000/task-management-frontend 211.157.146.6:5000/oauth-server-service 211.157.146.6:5000/ambassador 
+docker kill task-management-api oauth-server-service task-management-frontend ambassador;docker rm task-management-api oauth-server-service task-management-frontend ambassador;
+docker rmi 211.157.146.6:5000/task-management-api 211.157.146.6:5000/task-management-frontend 211.157.146.6:5000/oauth-server-service
 
-docker run -d --name=ambassador --expose 27017 --expose 5432 --expose 4379 -e MONGO_PORT_27017_TCP=tcp://172.16.2.25:27017  -e DB_PORT_5432_TCP=tcp://172.16.2.25:5432 -e REDIS_PORT_4379_TCP=tcp://172.16.2.25:4379 211.157.146.6:5000/ambassador:latest
+docker run -d --name=ambassador --expose 27017 --expose 5432 --expose 6379 -e MONGO_PORT_27017_TCP=tcp://172.16.2.25:27017  -e DB_PORT_5432_TCP=tcp://172.16.2.25:5432 -e REDIS_PORT_6379_TCP=tcp://172.16.2.25:6379 211.157.146.6:5000/ambassador:latest
 docker run -d -p 6009:80 --name=task-management-frontend 211.157.146.6:5000/task-management-frontend
 docker run -d -p 6002:80 --link ambassador:db --name=oauth-server-service 211.157.146.6:5000/oauth-server-service
 docker run -d -p 6001:6001 --link ambassador:mongo --link ambassador:redis --link oauth-server-service:oauth --name=task-management-api 211.157.146.6:5000/task-management-api
